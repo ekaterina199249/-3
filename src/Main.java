62efc8aaca43c27dd903b63ff31f7ed6c08a160c
Admin <ekaterina28041998@gmail.com>
2025-01-17 23:21:09 +0300

    Домашнее задание 3. Доработка

--- src/Main.java
+++ src/Main.java
@@ -10,18 +10,16 @@ public class Main {
 
         System.out.println("/nTask 2");
         int airTemperature = 6;
-        if (airTemperature < 5) {
+        if (airTemperature <= 5) {
             System.out.println("На улице " + airTemperature + " градусов, нужно надеть шапку");
-        }
-        if (airTemperature > 5) {
+        }else{
             System.out.println("На улице " + airTemperature + " градусов, можно идти без шапки");
         }
         System.out.println("/nTask 3");
         int speed = 110;
         if (speed > 60) {
             System.out.println("Если скорость " + speed + ", то придется заплатить штраф");
-        }
-        if (speed < 60) {
+        } else {
             System.out.println("Если скорость " + speed + "то можно ехать спокойно");
         }
         System.out.println("/nTask 4");
@@ -33,18 +31,17 @@ public class Main {
         if (age >= 7 && age <= 17) {
             System.out.println("Если человеку " + age + ", то ему нужно ходить в школу");
         }
-        if (age >= 18 && age <= 24) {
+        if (age >= 18 && age < 24) {
             System.out.println("Если человеку " + age + ", то его место в университете");
-        }
-        if (age >= 24) {
+        } else if (age >= 24){
             System.out.println("Если человеку " + age + ", то ему пора ходить на работу");
         }
         System.out.println("/nTask 5");
-        age = 8;
+        age = 5;
         if (age < 5) {
             System.out.println("Если возраст ребенка меньше " + age + ", то он не может кататься на аттракционах");
         }
-        if (age > 5 && age < 14) {
+        if (age >= 5 && age <= 14) {
             System.out.println("Если возраст ребенка равен " + age + ", то он может кататься только в сопровождении взрослого. Если взрослого нет, то кататься нельзя.");
         }
         if (age > 14) {
